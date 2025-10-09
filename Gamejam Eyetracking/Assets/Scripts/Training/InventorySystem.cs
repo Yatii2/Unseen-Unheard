96f16b703b43aa44bca430f8f056db1357f12a2a
37955 <pjotrart@gmail.com>
2025-10-09 10:20:15 +0200

    Opdracht BA

---
+++ Gamejam Eyetracking/Assets/Scripts/Training/InventorySystem.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    private Dictionary<string, int> inventory = new Dictionary<string, int>();
+
+    public void AddItem(string itemName)
+    {
+        // Implementeer hier je logica
+    }
+
+    public void UseItem(string itemName)
+    {
+        // Implementeer hier je logica
+    }
+}
